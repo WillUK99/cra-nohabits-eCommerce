f12fd4116b125d0a0ade1e272f5f6f481a2177a8
WillUK99 <willuk1999@gmail.com>
2022-04-02 09:53:16 +0100

    Writing shop data to firestore cloud db

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -1,6 +1,8 @@
 import React, { createContext, useState, useEffect } from 'react'
 
-import PRODUCTS from "../shop-data.json" // mock data
+import { addCollectionAndDocuments, getCategoriesAndDocuments } from '../utils/firebase/firebase.utils.js'
+
+import SHOP_DATA from "../shop-data.js" // mock data
 
 export const ProductsContext = createContext({
   // the initial state of the context should be empty/null
@@ -9,13 +11,21 @@ export const ProductsContext = createContext({
 })
 
 export const ProductsProvider = ({ children }) => {
-  const [products, setProducts] = useState(PRODUCTS) // setting intial state of products from our mock data
+  const [products, setProducts] = useState([]) // setting intial state of products from our mock data
   const value = { products, setProducts } // 'value' allows us to acces the products/setProducts from the context object
 
   // useEffect(() => {
-  //   console.log("ProductProvider: useEffect")
-    
+  //   // setting shopdata to firebase
+  //    addCollectionAndDocuments('categories', SHOP_DATA)
   // }, [])
+   
+  useEffect(() => {
+    const getCategoriesMap = async () => {
+      const categoriesMap = await getCategoriesAndDocuments()
+      // console.log(categoriesMap)
+    }
+    getCategoriesMap()
+  }, [])
 
   return (
     <ProductsContext.Provider value={value}> {/*this is the value you want to access from the context*/}
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -13,7 +13,11 @@ import {
   getFirestore,
   doc,
   getDoc,
-  setDoc
+  setDoc,
+  collection,
+  writeBatch,
+  query,
+  getDocs,
 } from "firebase/firestore"
 
 // Web app Firebase configuration
@@ -40,21 +44,70 @@ export const signInWithGooglePopup = () => signInWithPopup(auth, googleProvider)
 
 export const db = getFirestore()
 
+// Allows us to write to the firebase db with a batch of Objects -> see product.context.jsx ~line 18
+export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+  // Get a reference to the collection
+  const collectionRef = collection(db, collectionKey)
+  // Start a batch
+  const batch = writeBatch(db)
+  // Loop through the object we want to add
+  objectsToAdd.forEach((obj) => {
+    // Create a new doc ref for each new object we want to add
+    const newDocRef = doc(collectionRef, obj.title.toLowerCase())
+    // Set the data to be written to the doc
+    batch.set(newDocRef, obj)
+  })
+  // Commit the batch
+  return await batch.commit()
+}
+
+export const getCategoriesAndDocuments = async () => {
+  // Get a reference to the collection we want to query
+  const collectionRef = collection(db, "categories")
+
+  const q = query(collectionRef)
+
+  const querySnapshot = await getDocs(q)
+  const categoryMap = querySnapshot.docs.reduce((acc, docSnapshot) => {
+    // Get the data from the docSnapshot in this case we want the title and items
+    const { title, items } = docSnapshot.data()
+    /**
+     * We want to return an object with the title as the key and the items as the value to the accumulator.
+     * in this case the value of the object is "items" is an object of the items in the category, the title is the key.
+     * In this case the value of the object we are creating on each iteration of the reducer will have its key as the title and the value as the items{}.
+     * 
+     * console.log will show the object we are creating on each iteration of the reducer.
+     */
+    acc[title.toLowerCase()] = items
+    // console.log(acc)
+    console.log(title, items)
+    // the acc is the object we want to return for each subsequent iteration of the reducer.
+    return acc
+  }, {})
+  
+  // return the categoryMap object
+  return categoryMap
+}
+
 export const createUserDocumentFromAuth = async (userAuth, additionalInformation) => {
   if (!userAuth) return
-  // Sets which collection we want to use, in this case the users collection, 
-  // and what the document will be called in this case the users uid
+  // Get a reference to the document we want to query
+  // We want to query the users collection. 
+  // And the userAuth.uid is the unique id of the user document we want to query
   const userDocRef = doc(db, "users", userAuth.uid)
   // console.log(userDocRef)
 
+  // Get the data from the document we want to query
   const userSnapshot = await getDoc(userDocRef)
   // console.log(userSnapshot)
 
+  // If the userSnapshot is empty then we want to create a new user document
   if (!userSnapshot.exists()) {
     const { displayName, email } = userAuth
     const createdAt = new Date()
 
     try {
+      // Create a new user document with the data we want to write to the document
       await setDoc(userDocRef, {
         displayName,
         email,
